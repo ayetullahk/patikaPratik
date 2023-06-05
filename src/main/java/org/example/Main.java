f11133d182f40a1fb429d941142291d63512b320
ayetullahkarabulut <karabulutayetullah@gmail.com>
2023-06-05 21:16:07 +0300

    commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n, total = 0;
+        Scanner inp = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı giriniz :");
+            n = inp.nextInt();
+            if (n % 4 == 0) {
+                total+=n;
+            }
+        }while (n%2==0);
+
+        System.out.println("Toplam :"+ total);
+    }
+}
